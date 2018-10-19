6387284d763d067d253d6c00eb5df1c046a86813
Reagan Middlebrook <reagankm@gmail.com>
2018-10-19 14:35:13 -0700

    Initial commit for GitHub repo

---
+++ src/Employment.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Row, Panel } from 'react-bootstrap';
+
+import './Employment.css';
+import './fonts.css';
+
+// company: string, image: imported file, title: string, and dates: string
+export function WorkExperience(props) {
+
+  return (
+      <Panel className='work-exp-panel ultra'>
+        <Row className='exp-company'>{props.company}</Row>
+        <Row className='exp-image'>
+          <img src={props.image} alt='' />
+        </Row>
+
+        <Row>{props.title}</Row>
+        <Row className='subtitle cinzel'>{props.dates}</Row>
+
+      </Panel>
+  );
+}
