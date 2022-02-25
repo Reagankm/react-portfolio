df3720eb51b6c71b0f4b5c0ab7d05086bfaf4dc0
Reagan Middlebrook <reagankm@gmail.com>
2022-02-24 16:29:48 -0800

    Render Skills in own class from data .json

--- src/experience/Experience.jsx
+++ src/experience/Experience.jsx
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col, Panel } from 'react-bootstrap';
+import { Grid } from 'react-bootstrap';
 
 import { renderEmploymentHistory } from './Employment';
 import { renderProjects } from './Project';
+import { renderSkills } from './Skills';
 
 import './Experience.css';
 import '../fonts.css';
 
-// heading: string, technology: string[]
-function Skills(props) {
 
-  return (
-    <div>
-      {props.heading}
-      <p className='bai' style={{fontFamily: props.font}}>
-        {props.technology.join(', ')}
-      </p>
-    </div>
-
-  );
-}
 
 class Experience extends Component {
 
@@ -36,32 +25,13 @@ class Experience extends Component {
         return (
             <Grid>
                 {renderEmploymentHistory()}
-                <Row className='heading ultra' id='skills'>
-                    Skills
-                </Row>
-                <Row>
-                    <Panel className='glass-panel ultra'>
-                        <Row>
-                            <Col xs={4}>
-                                <Skills heading='Top Skills' technology={['Java', 'Spring Boot', 'MongoDB', 'Git']}/>
-                            </Col>
-                            <Col xs={4}>
-                                <Skills heading='Secondary Skills'
-                                        technology={['Android', 'Unix Shell Scripting', 'Mockito', 'Lombok', 'Swagger', 'Gradle']}/>
-                            </Col>
-                            <Col xs={4}>
-                                <Skills heading="Other Tech I've Worked With"
-                                        technology={['Python', 'Dart', 'Flutter', 'C', 'C++', 'MySQL', 'PHP', 'Maven']}/>
-                            </Col>
-                        </Row>
-
-                    </Panel>
-                </Row>
+                {renderSkills()}
                 {renderProjects()}
             </Grid>
         );
     }
 
 }
+
 
 export default Experience;
