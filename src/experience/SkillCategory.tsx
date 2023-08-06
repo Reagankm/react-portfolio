da6099657c0cfb776b2712edd67936b34fdc1f6f
Reagan Middlebrook <reagankm@gmail.com>
2023-08-06 09:11:05 -0700

    Remove commented out lines

--- src/experience/SkillCategory.tsx
+++ src/experience/SkillCategory.tsx
@@ -1,14 +1,8 @@
 import React, { PropsWithChildren } from 'react';
-//import { Row, Col, Card } from 'react-bootstrap';
 
-//import skillData from './data/skills.json';
 import './Experience.css';
 import '../fonts.css';
 
-// type JsonSkill = {
-//     type: string,
-//     values: string[]
-// }
 
 type Props = PropsWithChildren<{
     heading: string,
@@ -30,30 +24,3 @@ const SkillCategory: React.FC<Props> = (props: Props) => {
 };
 
 export default SkillCategory;
-//
-// export const renderSkills = () => {
-//     return (
-//         <div>
-//             <Row className='heading ultra' id='skills'>
-//                 Skills
-//             </Row>
-//             <Row>
-//                 <Card className='glass-panel ultra'>
-//                     <Row>
-//                         {skillData.map((skillCategory: JsonSkill) => {
-//                             const heading: string = skillCategory.type;
-//                             const tech: string[] = skillCategory.values;
-//                             return (
-//                                 <Col xs={4}>
-//                                     <SkillCategory heading={heading}
-//                                             technology={tech}/>
-//                                 </Col>
-//                             );
-//                         })}
-//                     </Row>
-//
-//                 </Card>
-//             </Row>
-//         </div>
-//     );
-// }
