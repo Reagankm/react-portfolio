1644bb48934faee02b34c944914903c0b500b113
Reagan Middlebrook <reagankm@gmail.com>
2023-08-07 10:50:32 -0700

    Employment History responsiveness; new skills organization

--- src/experience/Experience.tsx
+++ src/experience/Experience.tsx
@@ -4,20 +4,18 @@ import { Container } from 'react-bootstrap';
 import '../fonts.css';
 import './Experience.css';
 
-import { EmploymentHistory } from './Employment';
-import NewEmploymentHistory from './NewEmploymentHistory';
+import EmploymentHistory from './EmploymentHistory';
 import { renderProjects } from './Project';
 import SkillDisplay from './SkillDisplay';
 
 const Experience = () => {
 
     return (
-        <Container className='experience-container'>
-            <NewEmploymentHistory />
-            {/*<EmploymentHistory />*/}
+        <div className='experience-container'>
+            <EmploymentHistory />
             <SkillDisplay />
             {renderProjects()}
-        </Container>
+        </div>
     );
 
 }
